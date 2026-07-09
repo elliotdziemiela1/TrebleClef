e6104f9d0f1fd15b4304594dc2be4a73e3d5e2e0
Your Name <elliotanthony9@gmail.com>
2026-07-09 13:30:38 -0500

    consolidated code. Combined selected note idx, note x locations, and score into new type EditorScore. Now control history of EditorScore. Note selection works.

--- src/Components/Editor.tsx
+++ src/Components/Editor.tsx
@@ -6,11 +6,48 @@ import { pixelsPerMeasureX, pixelsPerStaveY, staveStartX, staveStartY,
 	rendererWidth, measuresPerStave, measureWidthPadding } from '../engine/renderer'; // Will use these soon
 import { useHistory } from '../custom_hooks';
 
+type EditorScore = { 
+	score: Score, 
+	measureNoteLocations: number[][], 
+	selectedNoteIdx: number[] | undefined
+}
+
 interface EditorProps {
 	historySize : number
 }
 
+// calculates the offset within the effective measure (the measure excluding padding for first and last notes) 
+// for each note in the score. returns a 2D array where 
+// each subarray corresponds to a measure and contains the x offsets of the notes in that measure.
+// This function is only computed the first time a score is rendered.
+function getMeasureNoteXLocations(score : Score) : number[][] {
+	let measureNoteXLocations : number[][] = [[]];
+	for (let i = 0; i < score.measures.length; i++){
+		const selectedNotes = score.measures[i].notes;
+		measureNoteXLocations[i] = [];
+		measureNoteXLocations[i][0] = -(measureWidthPadding / 2); // the leftmost pixel of the whole measure
+		if (selectedNotes.length > 1){
+			measureNoteXLocations[i][1] = calcNoteWidth(selectedNotes[0].duration) / 2;
+		}
+		for (let j = 2; j < selectedNotes.length; j++){
+			// measureNoteXLocations[i][j] = measureNoteXLocations[i][j - 1] + (calcNoteWidth(selectedNotes[j - 1].duration) * 1.5) - (calcNoteWidth(selectedNotes[j].duration) / 2);
+			let nextLocation = measureNoteXLocations[i][j - 1] + (calcNoteWidth(selectedNotes[j - 2].duration) / 2) + (calcNoteWidth(selectedNotes[j - 1].duration) / 2);
+			measureNoteXLocations[i][j] = nextLocation;
+
+		}
+	}
+	return measureNoteXLocations;
+}
+
 export default function Editor({ historySize } : EditorProps) {
+	// New logic
+	// combine selectedNoteIdx with score into EditorScore
+	// make EditorScore[] reducer
+	// when an edit is made (not undo/redo), push new EditorScore to front and set
+	// selected note index to 0;
+
+
+
 	const [ historyIndex, historyIndexDispatch ] = useReducer((state, action) => {
 		if (action < 0 || action >= historySize){
 			console.log("history limit reached.");
@@ -18,52 +55,43 @@ export default function Editor({ historySize } : EditorProps) {
 		} 
 		return action;
 	}, 0);
+
 	const scoreContainerRef = useRef<HTMLDivElement>(null);
-	const [ score, setScore ] = useState<Score[]>([demoScore]); // array for history
-	const [ selectedNoteIdx, selectedNoteDispatch ] = useReducer((state, action : number[]) => {
-		debugger
-		if (!!state[historyIndex] && !!state[historyIndex].length)
-			changeNoteColor(state[historyIndex], "black");
-		const newState : number[][] = [...state];
-		newState[historyIndex] = action;
-		changeNoteColor(action, "blue");
-		return newState;
-	}, [[]] as number[][]); // array for history
 
-	// stores the x locations of the notes in the selected measure.
-	const measureNoteLocations = useRef<number[][]>([]); 
+	// array for current EditorScore and it's history. First element is oldest, last is newest.
+	const [ editorScores, editorScoresReducer ] = useReducer((state : EditorScore[], action : EditorScore) => {
+		// delete oldest EditorScore, and push newest EditorScore
+		return [...state.slice(1,historySize), action];
+	}, [{ score: demoScore, measureNoteLocations: getMeasureNoteXLocations(demoScore), selectedNoteIdx: undefined}] );
 
-	// calculates the offset within the effective measure (the measure excluding padding for first and last notes) 
-	// for each note in the score. returns a 2D array where 
-	// each subarray corresponds to a measure and contains the x offsets of the notes in that measure.
-	// This function is only computed the first time a score is rendered.
-	const getMeasureNoteXLocations = useCallback(() : number[][] => {
-		let measureNoteXLocations : number[][] = [[]];
-		for (let i = 0; i < score[historyIndex].measures.length; i++){
-			const selectedNotes = score[historyIndex].measures[i].notes;
-			measureNoteXLocations[i] = [];
-			measureNoteXLocations[i][0] = -(measureWidthPadding / 2); // the leftmost pixel of the whole measure
-			if (selectedNotes.length > 1){
-				measureNoteXLocations[i][1] = calcNoteWidth(selectedNotes[0].duration) / 2;
-			}
-			for (let j = 2; j < selectedNotes.length; j++){
-				// measureNoteXLocations[i][j] = measureNoteXLocations[i][j - 1] + (calcNoteWidth(selectedNotes[j - 1].duration) * 1.5) - (calcNoteWidth(selectedNotes[j].duration) / 2);
-				let nextLocation = measureNoteXLocations[i][j - 1] + (calcNoteWidth(selectedNotes[j - 2].duration) / 2) + (calcNoteWidth(selectedNotes[j - 1].duration) / 2);
-				measureNoteXLocations[i][j] = nextLocation;
+	// // array for current selected note and history
+	// const [ selectedNoteIdx, selectedNoteDispatch ] = useReducer((state, action : number[]) => {
+	// 	debugger
+	// 	if (!!state[historyIndex] && !!state[historyIndex].length)
+	// 		changeNoteColor(state[historyIndex], "black");
+	// 	const newState : number[][] = [...state];
+	// 	newState[historyIndex] = action;
+	// 	changeNoteColor(action, "blue");
+	// 	return newState;
+	// }, [[]] as number[][]); 
 
-			}
-		}
-		return measureNoteXLocations;
-	}, [score]);
-
-	function changeNoteColor(indicies : number[], color : string) {
-		setScore((prevScore) => {
-			const newScore = { ...prevScore };
-			newScore[historyIndex].measures[indicies[0]].notes[indicies[1]].color = color;
-			return newScore;
-		});
+	function changeSelectedNote(newIdx : number[]){
+		const newEditorScore : EditorScore = editorScores[historyIndex];
+		if (!!newEditorScore.selectedNoteIdx?.length)
+			newEditorScore.score.measures[newEditorScore.selectedNoteIdx[0]].notes[newEditorScore.selectedNoteIdx[1]].color = "black";
+		newEditorScore.selectedNoteIdx = newIdx;
+		newEditorScore.score.measures[newIdx[0]].notes[newIdx[1]].color = "blue";
+		editorScoresReducer(newEditorScore);
 	}
+	
+	// // change color of a note in the current score
+	// function changeNoteColor(indicies : number[], color : string) {
+	// 	const newEditorScore : EditorScore = editorScores[historyIndex];
+	// 	newEditorScore.score.measures[indicies[0]].notes[indicies[1]].color = color;
+	// 	editorScoresReducer(newEditorScore);
+	// }
 
+	// delete a note  in the current score
 	function deleteNote(idx : number[]) {
 		setScore((prevScore) => {
 			const newScore = { ...prevScore };
@@ -81,7 +109,7 @@ export default function Editor({ historySize } : EditorProps) {
 		const boundingRect = scoreContainerRef.current.getBoundingClientRect();
 		const scoreLeft = boundingRect.left + staveStartX + 7; // 7 is a fudge factor to account for the clef and stave padding
 		const scoreTop = boundingRect.top;
-		for (let i = 0; i < score[historyIndex].measures.length; i++){
+		for (let i = 0; i < editorScores[historyIndex].score.measures.length; i++){
 			const measureLeft = scoreLeft + clefPadding + (i%4 * pixelsPerMeasureX);
 			const measureRight = measureLeft + pixelsPerMeasureX;
 			const effectiveMeasureLeft = measureLeft + measureWidthPadding / 2;
@@ -90,30 +118,30 @@ export default function Editor({ historySize } : EditorProps) {
 			// if clicked inside of this measure
 			if (event.clientX > measureLeft && event.clientX < measureRight && event.clientY > measureTop && event.clientY < measureBottom){
 				// find the note that was clicked on
-				for (let j = measureNoteLocations.current[i].length - 1; j >= 0; j--){
-					if (event.clientX - effectiveMeasureLeft > measureNoteLocations.current[i][j]) {
-						selectedNoteDispatch([i,j]);
+				for (let j = editorScores[historyIndex].measureNoteLocations.length - 1; j >= 0; j--){
+					if (event.clientX - effectiveMeasureLeft > editorScores[historyIndex].measureNoteLocations[i][j]) {
+						changeSelectedNote([i,j]);
 						return true;
 					}
 				}
 			} 
 		}
 		return false;
-	}, [score]);
+	}, [editorScores]);
 
 
+	// rerender score when it changes, or history index changes
 	useLayoutEffect(() => {
 			if (scoreContainerRef.current) {
-				renderScore(scoreContainerRef.current, score[historyIndex]);
-				measureNoteLocations.current = getMeasureNoteXLocations();
+				renderScore(scoreContainerRef.current, editorScores[historyIndex].score);
+				// measureNoteLocations.current = getMeasureNoteXLocations();
 			}
-	}, [score]);
+	}, [editorScores, historyIndex]);
 
 	const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
-		if (event.key == "Backspace" && !!selectedNoteIdx?.length) {
-			// changeNoteColor(selectedNoteIdx, "black");
-			if (!!selectedNoteIdx[historyIndex])
-				deleteNote(selectedNoteIdx[historyIndex]);
+		// handle note deletion
+		if (event.key == "Backspace" && !!editorScores[historyIndex].selectedNoteIdx?.length) {
+			deleteNote(editorScores[historyIndex].selectedNoteIdx);
 		}
 	}
 
