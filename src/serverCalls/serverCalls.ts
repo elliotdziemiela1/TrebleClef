87012d9b100fd9c8b2776fd87812d1c8b4beb3f8
Your Name <elliotanthony9@gmail.com>
2026-07-29 23:13:08 -0500

    Server url env var was incorrectly named.

--- src/serverCalls/serverCalls.ts
+++ src/serverCalls/serverCalls.ts
@@ -1,7 +1,7 @@
 import { useAuthContext } from "../auth/AuthContext"
 import type { UserProfile } from "../types/types"
 
-const serverUrl = import.meta.env.DEV ? "http://localhost:3000/" : import.meta.env.SERVER_URL
+const serverUrl = import.meta.env.DEV ? "http://localhost:3000/" : (import.meta.env.VITE_SERVER_URL || "")
 
 
 export async function reserveUsernameBase(username: string, sessionToken: string) : Promise<boolean> {
